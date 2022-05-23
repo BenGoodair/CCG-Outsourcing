04b92d4c0ac45603d93380c65171f6f929a56e39
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2022-05-23 12:36:38 +0100

    Add files via upload

--- R/NHS_Outsourcing_and_Mortality_Package.R
+++ R/NHS_Outsourcing_and_Mortality_Package.R
@@ -468,7 +468,7 @@ Create_table_1 <- function(MyAnnualDataCCG) {
   FinalCBPS_mortalitysum$tidy$estimate <- FinalCBPS_mortalitysum$tidy$estimate
   
   
-  modelsummary(list("ln. Treatable Mortality [95\% ci]"=FinalFEsum,"p-value"=FinalFEsum,"ln. Treatable Mortality [95\% ci]"=FinalFDsum,"p-value"=FinalFDsum,"ln. Treatable Mortality [95\% ci]"=FinalCBPS_GPsum,"p-value"=FinalCBPS_GPsum,"ln. Treatable Mortality [95\% ci]"=FinalCBPS_mortalitysum,"p-value"=FinalCBPS_mortalitysum,"ln. Treatable Mortality [95\% ci]"=FinalMLMsum,"p-value"=FinalMLMsum),
+  modelsummary(list("ln. Treatable Mortality [.95 ci]"=FinalFEsum,"p-value"=FinalFEsum,"ln. Treatable Mortality [.95 ci]"=FinalFDsum,"p-value"=FinalFDsum,"ln. Treatable Mortality [.95 ci]"=FinalCBPS_GPsum,"p-value"=FinalCBPS_GPsum,"ln. Treatable Mortality [.95 ci]"=FinalCBPS_mortalitysum,"p-value"=FinalCBPS_mortalitysum,"ln. Treatable Mortality [.95 ci]"=FinalMLMsum,"p-value"=FinalMLMsum),
                coef_omit = "Intercept|dept|year", add_rows = rows, 
                coef_map=cm,fmt = 4, estimate = c("{estimate} [{conf.low}, {conf.high}]", "p.value","{estimate} [{conf.low}, {conf.high}]", "p.value","{estimate} [{conf.low}, {conf.high}]", "p.value","{estimate} [{conf.low}, {conf.high}]", "p.value","{estimate} [{conf.low}, {conf.high}]", "p.value"), statistic = NULL,
                notes = list('Table reports results from multivariate longitudinal regression models.',
@@ -477,7 +477,7 @@ Create_table_1 <- function(MyAnnualDataCCG) {
                             'For full model expressions see supplementary material (S.2,p.4)',
                             'Robust SEs are clustered at CCG level and use a bias-reduced linearization estimator (CR2)',
                             'Satterthwaite degrees of freedom used in MLM',
-                            'Demographic Control variables include: Degree education (\%), Managerail or professional occupation (\%), Ethnic minority (\%), Unemployment rate (\%) and Claimant Rate (\%)'),
+                            'Demographic Control variables include: Degree education (percent), Managerial or professional occupation (percent), Ethnic minority (percent), Unemployment rate (percent) and Claimant Rate (percent)'),
                output = "kable") %>%
     add_header_above(c(" ", "Fixed Effects" = 2, "First Differences" = 2, "Covariate Balancing (1)" = 2, "Covariate Balancing (2)" = 2, "Multi-Level Model" = 2))
     # tab_spanner(label = 'Fixed Effects', columns = 2:3) \%>\%
